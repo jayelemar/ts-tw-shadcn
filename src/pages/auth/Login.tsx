500da3e5b8e89f26053e3ec2eb0601f6f805066f
Jay Termulo <jetermulo@gmail.com>
2023-12-18 03:09:25 +0800

    build: initial project setup

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function Login() {
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    );
+}
+
+export default Login;
